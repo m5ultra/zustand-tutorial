991e6c511ac45bcd04c93eea25429b3856b4eccf
zhenfeng.wang01 <zhenfeng.wang01@lkcoffee.com>
2023-12-20 22:57:36 +0800

    Feat: 自定义事件

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useLayoutEffect } from 'react'
 import { BearBox } from './components/BearBox'
 import { CatBox } from './components/CatBox'
 import { CatBox2 } from './components/CatBox2.tsx'
@@ -5,6 +6,39 @@ import { CatController } from './components/CatController.tsx'
 import { FoodBox } from './components/FoodBox.tsx'
 
 function Root() {
+  const handleCustomClick = (e: Event): void => {
+    console.log('自定义事件', e)
+  }
+
+  const handleClick = () => {
+    console.log('按钮被点击')
+  }
+
+  useEffect(() => {
+    const customEvent = new CustomEvent('cusClick', {
+      bubbles: true,
+      cancelable: true,
+      composed: true,
+      detail: {
+        name: 'Dendi',
+        age: 80,
+      },
+    })
+
+    document.addEventListener('cusClick', handleCustomClick)
+    const btnEl = document.getElementById('btn')
+    console.log(btnEl, 'btnEl')
+    if (btnEl) {
+      btnEl.onclick = () => {
+        console.log('click')
+        btnEl?.dispatchEvent(customEvent)
+      }
+    }
+    return () => {
+      document.removeEventListener('cusClick', handleCustomClick)
+    }
+  }, [])
+
   return (
     <div className="container">
       <h1>Zustand Tutorial</h1>
@@ -13,10 +47,13 @@ function Root() {
         <FoodBox />
       </div>
       <div>
-        {/* 测试002 */}
+        {/* 测试002 */}console.log();
         <CatBox />
         <CatBox2 />
         <CatController />
+        <button id={'btn'} onClick={handleClick}>
+          Click me
+        </button>
       </div>
     </div>
   )
